852cd91fcc11d3e8a3f3cda8e44c3cc25bda1b6b
Morgan Lafferty <mclafferty@berkeley.edu>
2021-05-05 09:47:51 -0700

    cleaned up

--- recycleai2/RecycleAI_2.0/ImagePicker.swift
+++ recycleai2/RecycleAI_2.0/ImagePicker.swift
@@ -2,7 +2,7 @@
 //  File.swift
 //  RecycleAI_2.0
 //
-//  Created by Morgan Lafferty on 4/22/21.
+//  Created by Morgan Lafferty, Maylin van Cleeff, Kristine Pau on 4/22/21.
 //
 
 import SwiftUI
@@ -11,108 +11,6 @@ import CoreML
 import Vision
 import ImageIO
 
-
-/*struct ImagePicker: UIViewControllerRepresentable {
-    
-    var sourceType: UIImagePickerController.SourceType = .photoLibrary
-    @Binding var image: Image?
-    @Binding var isPresented: Bool
-    
-    func makeCoordinator() -> ImagePickerViewCoordinator {
-        return ImagePickerViewCoordinator(image: $image, isPresented: $isPresented)
-    }
-    
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let pickerController = UIImagePickerController()
-        pickerController.sourceType = sourceType
-        pickerController.delegate = context.coordinator
-        return pickerController
-    }
-
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
-        // Nothing to update here
-    }
-
-}
-
-class ImagePickerViewCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    
-    @Binding var image: Image?
-    @Binding var isPresented: Bool
-    
-    init(image: Binding<Image?>, isPresented: Binding<Bool>) {
-        self._image = image
-        self._isPresented = isPresented
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            self.image = Image(uiImage: image)
-        }
-        self.isPresented = false
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        self.isPresented = false
-    }
-    
-}*/
-
-
-
-
-/*
-struct ImagePicker: UIViewControllerRepresentable {
-    typealias SourceType = UIImagePickerController.SourceType
-
-    let sourceType: SourceType
-    
-    class Coordinator: NSObject,
-        UINavigationControllerDelegate,
-        UIImagePickerControllerDelegate {
-        let parent: ImagePicker
-        
-        
-        init(_ parent: ImagePicker) {
-            self.parent = parent
-        }
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            
-            if let uiImage = info[.originalImage] as?
-                UIImage {
-                parent.image = uiImage
-            }
-            parent.presentationMode.wrappedValue.dismiss()
-        }
-    }
-    
-    @Environment(\.presentationMode) var presentationMode
-    @Binding var image: UIImage?
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>)
-    -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.sourceType = sourceType
-        
-        /*if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            picker.sourceType = .camera
-        } else {
-            picker.sourceType = .photoLibrary
-        }*/
-       
-        return picker
-    }
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>)
-    {
-        
-    }
-}
- */
 
 struct imagePicker:UIViewControllerRepresentable {
     @Binding var image: UIImage?
